3c0c507fd3270f3869d27c9ea94889841be304cc
Vil <plugged@mail.ru>
2016-12-03 14:04:42 +0300

    start

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import { AppContainer } from 'react-hot-loader';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const rootEl = document.getElementById('root');
+ReactDOM.render(
+  <AppContainer>
+    <App />
+  </AppContainer>,
+  rootEl
+);
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    ReactDOM.render(
+      <AppContainer>
+         <App />
+      </AppContainer>,
+      rootEl
+    );
+  });
+}
