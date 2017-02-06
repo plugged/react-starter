06bb3704261f0b1b2f7da205a92d768d86168742
Vil <plugged@mail.ru>
2017-02-06 15:57:03 +0300

    moved routes to separate file

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,12 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
 import Layout from '../components/Layout';
-import Counter from '../components/Counter';
-import About from '../components/About';
-import NotFound from '../components/NotFound';
-
-
-// If you use React Router, make this component
-// render <Router> with your routes. Currently,
-// only synchronous routes are hot reloaded, and
-// you will see a warning from <Router> on every reload.
-// You can ignore this warning. For details, see:
-// https://github.com/reactjs/react-router/issues/2182
+import Routes from './Routes';
 
 export default class App extends Component {
   render() {
     return (
       <Layout>
-        <Switch>
-          <Route path="/" exact component={Counter} />
-          <Route path="/about" exact component={About} />
-          <Route component={NotFound} />
-        </Switch>
+        <Routes />
       </Layout>
     );
   }
---
+++ src/components/Routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Counter from '../components/Counter';
+import About from '../components/About';
+import NotFound from '../components/NotFound';
+
+const Routes = () => (
+  <Switch>
+    <Route path="/" exact component={Counter} />
+    <Route path="/about" exact component={About} />
+    <Route component={NotFound} />
+  </Switch>
+);
+
+export default Routes;
