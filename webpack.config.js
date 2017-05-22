cdb17c7bb2ab3801e8a59ab88858d90095df2804
Vil <plugged@mail.ru>
2017-05-22 23:30:45 +0300

    deps update

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,11 @@ module.exports = {
     modules: ['src', 'node_modules']
   },
   devtool: 'cheap-module-eval-source-map',
-  entry: ['react-hot-loader/patch', 'webpack-hot-middleware/client', './src/index'],
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-hot-middleware/client',
+    './src/index'
+  ],
   output: {
     path: path.resolve(__dirname, 'src'),
     filename: 'bundle.js',
@@ -48,7 +52,12 @@ module.exports = {
     }),
     new HappyPack({
       id: 'styles',
-      loaders: ['style-loader', 'css-loader?sourceMap', 'postcss-loader', 'sass-loader?sourceMap']
+      loaders: [
+        'style-loader',
+        'css-loader?sourceMap',
+        'postcss-loader',
+        'sass-loader?sourceMap'
+      ]
     })
   ].concat(runAnalyzer ? new BundleAnalyzerPlugin() : []),
   module: {
